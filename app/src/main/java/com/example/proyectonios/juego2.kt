1d6a3c55ba3f9f4159a11fafc2c84ba35f21ee7c
MynameisJorge04 <1bvpgjbg02@gmail.com>
2023-11-06 12:31:18 -0600

    Cmabios finales

--- app/src/main/java/com/example/proyectonios/juego2.kt
+++ app/src/main/java/com/example/proyectonios/juego2.kt
@@ -1,52 +1,57 @@
 package com.example.proyectonios
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.media.MediaPlayer
 import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
 import android.view.animation.AnimationUtils
-import android.view.animation.TranslateAnimation
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class juego1 : AppCompatActivity() {
+class juego2 : AppCompatActivity() {
 
     private val imageButtons = mutableListOf<ImageButton>()
     private var progresoSecuencia = 0
     private var mediaPlayer: MediaPlayer? = null
-    var niveles = 0
+    var niveles = 1
     private var buttonsClicked = 0
 
     private val recursosDeSonido = listOf(
-        R.raw.animal1,
-        R.raw.animal2,
-        R.raw.animal3,
-        R.raw.animal4,
-        R.raw.animal5,
-        R.raw.animal6
+        R.raw.grabacion1,
+        R.raw.grabacion2,
+        R.raw.grabacion3,
+        R.raw.grabacion4,
+        R.raw.grabacion5,
+        R.raw.grabacion6,
+        R.raw.grabacion7,
+        R.raw.grabacion8,
+        R.raw.grabacion9
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.juego1)
+        setContentView(R.layout.juego2)
 
-
-        imageButtons.add(findViewById(R.id.chooseAnimal1Button))
-        imageButtons.add(findViewById(R.id.chooseAnimal2Button))
-        imageButtons.add(findViewById(R.id.chooseAnimal3Button))
-        imageButtons.add(findViewById(R.id.chooseAnimal4Button))
-        imageButtons.add(findViewById(R.id.chooseAnimal5Button))
-        imageButtons.add(findViewById(R.id.chooseAnimal6Button))
+        imageButtons.add(findViewById(R.id.imageButton1))
+        imageButtons.add(findViewById(R.id.imageButton2))
+        imageButtons.add(findViewById(R.id.imageButton3))
+        imageButtons.add(findViewById(R.id.imageButton4))
+        imageButtons.add(findViewById(R.id.imageButton5))
+        imageButtons.add(findViewById(R.id.imageButton6))
+        imageButtons.add(findViewById(R.id.imageButton7))
+        imageButtons.add(findViewById(R.id.imageButton8))
+        imageButtons.add(findViewById(R.id.imageButton9))
 
         for (i in imageButtons.indices) {
             imageButtons[i].setOnClickListener {
-                onAnimalButtonClick(i)
+                onNumeroButtonClick(i)
             }
         }
+
     }
 
-    private fun onAnimalButtonClick(buttonIndex: Int) {
+    private fun onNumeroButtonClick(buttonIndex: Int) {
         if (progresoSecuencia < imageButtons.size) {
             val botonEsperado = imageButtons[progresoSecuencia]
 
@@ -55,8 +60,6 @@ class juego1 : AppCompatActivity() {
 
                 val soundResource = recursosDeSonido[buttonIndex]
                 playSound(soundResource)
-
-
 
                 // Desactivar el botón tocado
                 botonEsperado.isEnabled = false
@@ -70,16 +73,12 @@ class juego1 : AppCompatActivity() {
 
         if (buttonsClicked >= imageButtons.size) {
             stopMediaPlayer()
-            niveles = 1
+            niveles = 2
             val intent = Intent(this, secondpage::class.java)
             intent.putExtra("niveles", niveles) // Agrega el valor de niveles como un extra
             startActivity(intent)
         }
     }
-
-
-
-
 
     private fun playSound(soundResource: Int) {
         stopMediaPlayer()
@@ -97,4 +96,5 @@ class juego1 : AppCompatActivity() {
         super.onStop()
         stopMediaPlayer()
     }
+
 }
--- app/src/main/java/com/example/proyectonios/secondpage.kt
+++ app/src/main/java/com/example/proyectonios/secondpage.kt
@@ -1,11 +1,17 @@
 package com.example.proyectonios
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class secondpage : AppCompatActivity() {
 
@@ -13,7 +19,10 @@ class secondpage : AppCompatActivity() {
     var returnButton : ImageButton? = null
     var tituloBoton : ImageButton? = null
     var juego1Boton : ImageView? = null
-
+    var juego2Boton : ImageView? = null
+    var juego3Boton : ImageView? = null
+    var textView3 : TextView? = null // Agregado
+    var manoImagen : ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +32,55 @@ class secondpage : AppCompatActivity() {
         inicioButton = findViewById(R.id.inicioButton)
         tituloBoton = findViewById(R.id.tituloButton)
         juego1Boton = findViewById(R.id.juego1Boton)
+        manoImagen = findViewById(R.id.manoAnimada2)
+
+        val slideUpDownAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up_down)
+        manoImagen?.startAnimation(slideUpDownAnimation)
+
+        val niveles = intent.getIntExtra("niveles", 0) // 0 es el valor por defecto si no se encuentra el extra
+
+        if (niveles >= 1) {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog_congratulations)
+            textView3 = findViewById(R.id.textView3)
+            // Actualiza el TextView
+            textView3?.text = "Desbloqueaste nivel 2"
+            dialog.setCancelable(false)
+
+            val closeButton = dialog.findViewById<Button>(R.id.continuarButton)
+            closeButton.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.show()
+
+            juego2Boton = findViewById(R.id.juego2Boton)
+            juego2Boton?.setImageResource(R.drawable.group_7_abierto)
+
+
+            if (niveles >= 2) {
+                val dialog = Dialog(this)
+                dialog.setContentView(R.layout.dialog_congratulations)
+                // Actualiza el TextView
+                textView3?.text = "Desbloqueaste nivel 3"
+                dialog.setCancelable(false)
+
+                val closeButton = dialog.findViewById<Button>(R.id.continuarButton)
+                closeButton.setOnClickListener {
+                    dialog.dismiss()
+                }
+
+                dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                dialog.show()
+
+                juego3Boton = findViewById(R.id.juego3Boton)
+                juego3Boton?.setImageResource(R.drawable.group_8_abierto)
+
+
+            }
+        }
+
     }
 
     fun returnButton(view: View) {
@@ -42,6 +100,11 @@ class secondpage : AppCompatActivity() {
 
     fun juego1Boton(view: View) {
         val intent = Intent(this, juego1::class.java)
+        startActivity(intent)
+    }
+
+    fun juego2Boton(view: View) {
+        val intent = Intent(this, juego2::class.java)
         startActivity(intent)
     }
 }
